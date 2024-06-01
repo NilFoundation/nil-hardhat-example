7173a81e78655ee8bf210267b983dddf9d9cd6cd
Aleksao998 <aleksaopacic988@gmail.com>
2024-06-01 12:42:38 +0200

    init project

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 private value;
+
+    event ValueChanged(uint256 newValue);
+
+    function increment() public {
+        value += 1;
+        emit ValueChanged(value);
+    }
+
+    function getValue() public view returns (uint256) {
+        return value;
+    }
+}
