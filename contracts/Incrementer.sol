e3e1963e85130a15f16d38ca4fe296aacd574315
rolaman <rolamanbuss@yandex.ru>
2024-07-04 15:54:44 +0400

    npm

--- contracts/Incrementer.sol
+++ contracts/Incrementer.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.0;
 
 contract Incrementer {
-    uint256 private value;
+    uint256 public value;
+
+    constructor() payable {
+    }
 
     event ValueChanged(uint256 newValue);
 
-    function increment() public {
+    function increment() public payable {
         value += 1;
         emit ValueChanged(value);
     }
