9d3b2fc1afb89b99a5c101cf6c52b51358ca35ec
Shree Vatsa <vastas2002@gmail.com>
2024-10-30 12:00:01 +0530

    Removed unused variables

--- hardhat-examples/test/SendRequest.ts
+++ hardhat-examples/test/SendRequest.ts
@@ -4,8 +4,6 @@ import "@nomicfoundation/hardhat-ethers";
 import { deployNilContract } from "../src/deployUtil";
 
 describe("Requester and Increment contract interaction", () => {
-    let callerAddress: string;
-    let incrementerAddress: string;
 
     it("Should Deploy Requester with shardId 2, deploy Incrementer with shardId 1, and call incrementer from caller using sendRequest", async function () {
         this.timeout(1200000);
@@ -14,8 +12,7 @@ describe("Requester and Increment contract interaction", () => {
         // Deploy Caller contract
         const { deployedContract: sendRequester, contractAddress: callerAddr } =
             await deployNilContract("SendRequest");
-        callerAddress = callerAddr;
-        console.log("Caller deployed at:", callerAddress);
+        console.log("Caller deployed at:", callerAddr);
 
         // Set shardId back to 1
         hre.config.shardId = 1;
@@ -23,15 +20,14 @@ describe("Requester and Increment contract interaction", () => {
         // Deploy Incrementer contract
         const { deployedContract: incrementer, contractAddress: incrementerAddr } =
             await deployNilContract("Incrementer");
-        incrementerAddress = incrementerAddr;
-        console.log("Incrementer deployed at:", incrementerAddress);
+        console.log("Incrementer deployed at:", incrementerAddr);
         // Increment the value
         await incrementer.increment();
         // Check the value of Incrementer
         expect(await incrementer.getValue()).to.equal(1);
 
         // Call the Caller contract's call method with the Incrementer address
-        await sendRequester.call(incrementerAddress);
+        await sendRequester.call(incrementerAddr);
 
         let value = await sendRequester.values("RANDOM_STORAGE");
         console.log("returned value: ", value);
