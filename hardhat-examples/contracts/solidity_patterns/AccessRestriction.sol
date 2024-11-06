39e1b45be12b6066dab16f9341fe5bdfd0de13ff
EvgeniiPestriakov <utren95@gmail.com>
2024-11-06 21:16:56 +0500

    added access restriction pattern and state machine deleted

---
+++ hardhat-examples/contracts/solidity_patterns/AccessRestriction.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AccessRestriction {
+    address public owner;
+    mapping(address => bool) public admins;
+
+    uint public controlValue = 0;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Access denied: Only owner");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(admins[msg.sender], "Access denied: Only admin");
+        _;
+    }
+
+    function addAdmin(address admin) external onlyOwner {
+        require(admin != address(0), "Invalid address");
+        admins[admin] = true;
+    }
+
+    function removeAdmin(address admin) external onlyOwner {
+        require(admins[admin], "Address is not an admin");
+        admins[admin] = false;
+    }
+
+    // Пример функции, доступной только для администраторов
+    function accessRestrictionAction() external onlyAdmin {
+        controlValue += 1;
+    }
+}
---
+++ hardhat-examples/test/solidity_patterns_tests/AccessRestriction.ts
@@ -0,0 +1,32 @@
+import {deployNilContract} from "../../src/deployUtil";
+import {expect} from "chai";
+import hre from "hardhat";
+
+describe("Access Restriction test", () => {
+  it("positive_scenario", async () => {
+    const {deployedContract: ar, contractAddress: address} = await deployNilContract("AccessRestriction");
+
+    expect(await ar.controlValue()).to.equal(0)
+    await ar.addAdmin(ar.owner())
+    await ar.accessRestrictionAction()
+    expect(await ar.controlValue()).to.equal(1)
+  });
+  it("access_restricted_scenario", async () => {
+    const {deployedContract: ar} = await deployNilContract("AccessRestriction");
+
+    expect(await ar.controlValue()).to.equal(0)
+    await ar.accessRestrictionAction()
+    expect(await ar.controlValue()).to.equal(0)
+  });
+  it("admin_excluded_from_pool", async function () {
+    this.timeout(60000);
+
+    const {deployedContract: ar} = await deployNilContract("AccessRestriction");
+
+    await ar.addAdmin(ar.owner())
+    await ar.accessRestrictionAction()
+    await ar.removeAdmin(ar.owner())
+    await ar.accessRestrictionAction()
+    expect(await ar.controlValue()).to.equal(1)
+  });
+})
